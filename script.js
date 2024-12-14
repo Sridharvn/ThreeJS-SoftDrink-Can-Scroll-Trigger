b96cfa3921a9c58ca6d30967a89883286dcdb3f6
Sridharvn <sridharvnampoothiripad@gmail.com>
2024-12-14 17:36:36 +0530

    Added renderer

--- script.js
+++ script.js
@@ -3,4 +3,29 @@ lenis.on("scroll", ScrollTrigger.update);
 gsap.ticker.add(time => {
     lenis.raf(time * 1000)
 })
-gsap.ticker.lagSmoothing(0)
+gsap.ticker.lagSmoothing(0);
+
+const scene = new THREE.Scene();
+
+scene.background = new THREE.Color(0xfefdfd);
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true
+});
+renderer.setClearColor(0xffffff, 1);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.physicallyBCorrectLights = true;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 2.5;
+document.querySelector(".model").appendChild(renderer);
